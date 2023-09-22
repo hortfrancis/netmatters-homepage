432a1779ce229af720fe62079d99b2a3fbf213f5
Alex Hort-Francis <ajhf@hotmail.co.uk>
2023-09-22 14:55:56 +0100

    Add basic functionality to secondary-nav
    - secondary-nav accessible via burger button in header
    - Clicking on main page content hides secondary-nav again
    - Rename hamburger button in header to .header__nav-menu-button
    - Move .sidebar styling rules to .secondary-nav

---
+++ js/secondaryNav.js
@@ -0,0 +1,42 @@
+/*
+    Handle secondary-nav in the `<aside>` that appears to the right of the page
+*/
+
+const navMenuButton = document.querySelector(".header__nav-menu-button");
+const secondaryNav = document.querySelector(".secondary-nav");
+const main = document.querySelector("main");
+
+// When the burger button in the header is clicked:
+navMenuButton.addEventListener("click", event => {
+
+    // Add minor layout styling rules to make secondary-nav clickable & scrollable
+    secondaryNav.classList.add("secondary-nav--active");
+
+    // Shift the main page content to the right to reveal secondary-nav
+    main.classList.add("main--secondary-nav-active");
+
+    // (`darkOverlay` is defined in `cookiesPopUp.js`)
+    darkOverlay.classList.add('dark-overlay--active');
+
+    // Prevent scrolling down the rest of the site
+    body.style.overflow = 'hidden';
+
+    // `<main>` essentially becomes the button to close the secondary nav,
+    // so add a cursor pointer to make this more obvious
+    darkOverlay.style.cursor = "pointer";
+});
+
+darkOverlay.addEventListener("click", event => {
+
+    // Move the main page content back to its normal position to hide secondary-nav
+    main.classList.remove("main--secondary-nav-active");
+
+    // Remove the 'greyed out' effect from the rest of the site
+    darkOverlay.classList.remove('dark-overlay--active');
+
+    // Push the secondary-nav back behind the `<main>` page content
+    secondaryNav.classList.remove("secondary-nav--active");
+
+    // Allow scrolling down the rest of the site
+    body.style.overflow = 'auto';
+});
